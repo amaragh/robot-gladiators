78aff02844c91d6fb912b3afb09202d6054c9cdf
amaragh <mercer.alecia@gmail.com>
2022-08-24 22:39:49 -0500

    save high score using localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -43,59 +43,59 @@ var fight = function (enemy) {
 
         // ask the player if they'd like to fight or skip using the fightOrSkip function
         if (isPlayerTurn) {
-        if (fightOrSkip()) {
-            // if true, leave fight by breaking loop
-            break;
-        }
+            if (fightOrSkip()) {
+                // if true, leave fight by breaking loop
+                break;
+            }
 
-        // generate random damage value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-        // Subtract the value of 'damage' from the value of 'enemy.health' and use that result to update the value in the 'enemy.health' variable
-        enemy.health = Math.max(0, enemy.health - damage);
+            // Subtract the value of 'damage' from the value of 'enemy.health' and use that result to update the value in the 'enemy.health' variable
+            enemy.health = Math.max(0, enemy.health - damage);
 
-        // Log a resulting message to the console so we know that it worked
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );
+            // Log a resulting message to the console so we know that it worked
+            console.log(
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+            );
 
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died.");
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died.");
 
-            // award player money for winning
-            playerInfo.money = playerInfo.money + 20;
-            // leave while() loop since enemy is dead
-            break;
-        }
-        else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
-        // player gets attacked first
-    } else {
-
-        // generate random damage value based on enemy's attack power
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
-        // Subtract the value of 'damage' from the value of 'playerInfo.health' and use that results to update the value in the 'playerInfo.health' variable
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        );
-
-        // check player's health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died.");
-            // leave while() loop if player has died
-            break;
+                // award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+                // leave while() loop since enemy is dead
+                break;
+            }
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+            // player gets attacked first
         } else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+
+            // generate random damage value based on enemy's attack power
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+            // Subtract the value of 'damage' from the value of 'playerInfo.health' and use that results to update the value in the 'playerInfo.health' variable
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+
+            // Log a resulting message to the console so we know that it worked.
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+            );
+
+            // check player's health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died.");
+                // leave while() loop if player has died
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
-    }
-    // switch turn order for next round
-    isPlayerTurn = !isPlayerTurn;
+        // switch turn order for next round
+        isPlayerTurn = !isPlayerTurn;
 
     } // end of while loop
 }; // end of fight function
@@ -147,13 +147,25 @@ var startGame = function () {
 
 // function to end the entire game
 var endGame = function () {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore){
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+
 
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
